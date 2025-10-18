aa18a4261f587a6db40617cce567338086b53368
Aleksandra Serafimskaya <a79256103066@gmail.com>
2025-10-18 17:56:26 +0300

    initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,13 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Post {
+    public String name;// поле для имени
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+
+
+}
